950ec43c3ab778ce0fd318bb4471464113538004
nitin-ppnp <nitin.ppnp@gmail.com>
2021-10-10 17:33:24 +0000

    Deploying to gh-pages from  @ 9b288352ebe180c97f2f585f80e624ecf92df5f4 🚀

--- assets/index/code/postcards.jl
+++ assets/index/code/postcards.jl
@@ -1,7 +1,8 @@
 # This file was generated, do not modify it. # hide
 #hideall
 posts = [
-  (name="OpenCLBoostCompute", pic="BoostCompute.png", title="OpenCL using Boost Compute",link="/OpenCLBoostCompute/")
+  (name="OpenCLBoostCompute", pic="BoostCompute.png", title="OpenCL using Boost Compute",link="/OpenCLBoostCompute/"),
+  (name="AMASSContacts", pic="smplconts/card_img.gif", title="Generating contact points for AMASS dataset",link="/AMASSContacts")
   ]
 
 "@@cards @@row" |> println
